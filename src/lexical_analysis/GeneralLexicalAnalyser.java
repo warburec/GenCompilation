751ac633716c8f806194661039bd729a01305acd
warburec <77669019+warburec@users.noreply.github.com>
2023-06-13 12:01:45 +0100

    reserved words types/documentation

--- src/lexical_analysis/GeneralLexicalAnalyser.java
+++ src/lexical_analysis/GeneralLexicalAnalyser.java
@@ -6,7 +6,7 @@ import grammar_objects.*;
 
 public class GeneralLexicalAnalyser implements LexicalAnalyser {
 
-    /*
+    /** 
         Initial checking:
         User-definables must have mutually exclusive regex if used in identical rules
 
@@ -23,12 +23,17 @@ public class GeneralLexicalAnalyser implements LexicalAnalyser {
             Approaches:
                 Pure lexical analysis
                 Grammar assisted analysis (will be a lot slower)
+                [These need comparing, to make seperate analysers]
+
+        @param whitespaceDelimiters All string to be considered as whitespace (Will not be tokenised)
+        @param stronglyReservedWords Words that cannot be part of a user-definable token. Examples: operators, punctuation, etc. (Not including whitespace)
+        @param weaklyReservedWords Words that will be tokenised if they do not appear as part of a user-definable token
     */
     GeneralLexicalAnalyser(
-        Token[] punctuation,
-        Token[] operators,
-        String whitespaceDelimiters, //Could make this an array of strings if specific strings are considered whitespace/delimiters
-        ProductionRule[] grammaRules,
+        String[] whitespaceDelimiters, //Could make this an array of strings if specific strings are considered whitespace/delimiters
+        Token[] stronglyReservedWords,
+        Token[] weaklyReservedWords,
+        ProductionRule[] grammarRules,
         Map<Token, String> customTokenRegex,
         Map<Literal, String> literalRegex
     ) {
