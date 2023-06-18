b18fe4376d2ea8c42cfd026c1e51016c45965b84
warburec <77669019+warburec@users.noreply.github.com>
2023-06-18 15:24:44 +0100

    strong words only test

--- tests/GeneralLexicalAnalyserTests.java
+++ tests/GeneralLexicalAnalyserTests.java
@@ -162,4 +162,68 @@ public class GeneralLexicalAnalyserTests {
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void strongWordsOnly() {
+        String sentence =
+        "()((}}:::{:()}";
+
+        String[] delims = new String[] {
+            " ",
+            "\t",
+            "\r\n",
+            "\n"
+        };
+
+        String[] stronglyReserved = new String[] {
+            "(",
+            ")",
+            ":",
+            "{",
+            "}"
+        };
+
+        String[] weaklyReserved = new String[] {
+            "for"
+        };
+
+        Map<String, NotEmptyTuple<String, String>> dynamicTokenRegex = new HashMap<String, NotEmptyTuple<String, String>>();
+        dynamicTokenRegex.put("[^\"0-9].*", new NotEmptyTuple<String, String>("Identifier", "identifier")); //TODO: Reformat to Regex:<Class, grammarName> 
+        dynamicTokenRegex.put("\".*\"", new NotEmptyTuple<String, String>("Literal", "string"));
+        dynamicTokenRegex.put("[0-9]+[\\.[0.9]+]?", new NotEmptyTuple<String, String>("Literal", "number"));
+        LexicalAnalyser lexAnalyser = new GeneralLexicalAnalyser(
+            delims,
+            stronglyReserved,
+            weaklyReserved,
+            dynamicTokenRegex
+        );
+        
+
+        Token[] actual = lexAnalyser.analyse(sentence);
+
+
+        Token[] expected = new Token[] {
+            new Token("(", 1, 1),
+            new Token(")", 1, 2),
+            new Token("(", 1, 3),
+            new Token("(", 1, 4),
+            new Token("}", 1, 5),
+            new Token("}", 1, 6),
+            new Token(":", 1, 7),
+            new Token(":", 1, 8),
+            new Token(":", 1, 9),
+            new Token("{", 1, 10),
+            new Token(":", 1, 11),
+            new Token("(", 1, 12),
+            new Token(")", 1, 13),
+            new Token("}", 1, 14),
+        };
+
+        assertArrayEquals(expected, actual);
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i].getLineNumber(), actual[i].getLineNumber());
+            assertEquals(expected[i].getColumnNumber(), actual[i].getColumnNumber());
+        }
+    }
 }
