1c6bb28532c649ba52b8716ddadec72d152ee32d
warburec <77669019+warburec@users.noreply.github.com>
2023-07-05 19:49:28 +0100

    Test grammar design change (Needs improvement)

--- tests/test_aids/grammar_generators/Grammar.java
+++ tests/test_aids/grammar_generators/Grammar.java
@@ -2,6 +2,7 @@ package tests.test_aids.grammar_generators;
 
 import java.util.*;
 
+import code_generation.Generator;
 import grammar_objects.*;
 import tests.test_aids.GrammarParts;
 
@@ -11,8 +12,30 @@ public abstract class Grammar {
     protected List<NonTerminal> nonTerminals = new ArrayList<>();
     protected List<ProductionRule> productionRules = new ArrayList<>();
     protected NonTerminal sentinal;
+
+    private Map<String, Map<String, String>> codeGenerations = new HashMap<>();                          //Language, <Sentence, Code>
+    private Map<String, Map<String, Map<ProductionRule, Generator>>> ruleConvertorMap = new HashMap<>(); //Language, <Sentence, ruleConverterMap>
+    private Map<String, Map<String, String[]>> generationBookendMap = new HashMap<>();                   //Language, <Sentence, {preGeneration, postGeneration}>
     
-    public Grammar() {}
+    public Grammar() {
+        setUpTokens(tokens);
+        sentinal = setUpSentinal();
+        setUpNonTerminals(nonTerminals);
+        setUpProductionRules(productionRules);
+
+        setUpRuleConvertors(ruleConvertorMap);
+        setUpCodeGenerations(codeGenerations);
+        setUpGenerationBookends(generationBookendMap);
+    }
+
+    protected abstract void setUpTokens(List<Token> tokens);
+    protected abstract NonTerminal setUpSentinal();
+    protected abstract void setUpNonTerminals(List<NonTerminal> nonTerminals);
+    protected abstract void setUpProductionRules(List<ProductionRule> productionRules);
+
+    public ProductionRule getRule(int index) {
+        return productionRules.get(index);
+    }
 
     public GrammarParts getParts() {
         return new GrammarParts(
@@ -21,6 +44,65 @@ public abstract class Grammar {
                         Set.copyOf(productionRules), 
                         sentinal
                 );
+    }
+
+    protected abstract void setUpGenerationBookends(Map<String, Map<String, String[]>> generationBookendMap);
+
+    public String[] getGenerationBookends(String sentence, String language) {
+        String[] bookends = null;
+
+        try {
+            bookends = generationBookendMap.get(language).get(sentence);
+        }
+        catch (NullPointerException e) {
+            throw new UnsupportedSentenceException("langage and generation bookends", sentence);
+        }
+
+        if(bookends == null) {
+            throw new UnsupportedSentenceException("generation bookends", sentence);
+        }
+
+        return bookends;
+    }
+
+
+    protected abstract void setUpRuleConvertors(Map<String, Map<String, Map<ProductionRule, Generator>>> ruleConvertorMap);
+
+    public Map<ProductionRule, Generator> getRuleConvertor(String sentence, String language) {
+        Map<ProductionRule, Generator> ruleConvertor = null;
+
+        try {
+            ruleConvertor = ruleConvertorMap.get(language).get(sentence);
+        }
+        catch (NullPointerException e) {
+            throw new UnsupportedSentenceException("langage and rule convertor", sentence);
+        }
+
+        if(ruleConvertor == null) {
+            throw new UnsupportedSentenceException("rule convertor", sentence);
+        }
+
+        return ruleConvertor;
+    }
+
+
+    protected abstract void setUpCodeGenerations(Map<String, Map<String, String>> codeGenerations);
+
+    public String getGeneratedCode(String sentence, String language) {
+        String generatedCode = null;
+
+        try {
+            generatedCode = codeGenerations.get(language).get(sentence);
+        }
+        catch (NullPointerException e) {
+            throw new UnsupportedSentenceException("langage and code generation", sentence);
+        }
+
+        if(generatedCode == null) {
+            throw new UnsupportedSentenceException("code generation", sentence);
+        }
+
+        return generatedCode;
     }
 
 }
--- tests/test_aids/grammar_generators/LR0TestGrammar.java
+++ tests/test_aids/grammar_generators/LR0TestGrammar.java
@@ -2,27 +2,18 @@ package tests.test_aids.grammar_generators;
 
 import java.util.*;
 
-import code_generation.Generator;
 import grammar_objects.*;
 import syntax_analysis.grammar_structure_creation.*;
 import syntax_analysis.parsing.*;
 
-//TODO: Seperate LR0 features and grammar features
+//TODO: Update for consistancy with SLR1TestGrammar
 public abstract class LR0TestGrammar extends Grammar {
     private List<State> states = new ArrayList<>();
-    private Map<String, Map<String, String>> codeGenerations = new HashMap<>();                          //Language, <Sentence, Code>
-    private Map<String, Map<String, Map<ProductionRule, Generator>>> ruleConvertorMap = new HashMap<>(); //Language, <Sentence, ruleConverterMap>
-    private Map<String, Map<String, String[]>> generationBookendMap = new HashMap<>();                   //Language, <Sentence, {preGeneration, postGeneration}>
 
     private Map<State, Map<Token, Action>> actionTable = new HashMap<>();
     private Map<State, Map<NonTerminal, State>> gotoTable = new HashMap<>();
 
     public LR0TestGrammar() {
-        setUpTokens(tokens);
-        sentinal = setUpSentinal();
-        setUpNonTerminals(nonTerminals);
-        setUpProductionRules(productionRules);
-
         setUpStates(states, new ProductionRule(null, new LexicalElement[] {sentinal}));
 
         for (State state : states) {
@@ -31,22 +22,6 @@ public abstract class LR0TestGrammar extends Grammar {
         }
         setUpActionTable(actionTable, new EOF());
         setUpGotoTable(gotoTable);
-
-        setUpRuleConvertors(ruleConvertorMap);
-        setUpCodeGenerations(codeGenerations);
-        setUpGenerationBookends(generationBookendMap);
-    }
-
-
-    protected abstract void setUpTokens(List<Token> tokens);
-    protected abstract NonTerminal setUpSentinal();
-    protected abstract void setUpNonTerminals(List<NonTerminal> nonTerminals);
-    
-
-    protected abstract void setUpProductionRules(List<ProductionRule> productionRules);
-    
-    public ProductionRule getRule(int index) {
-        return productionRules.get(index);
     }
 
 
@@ -81,63 +56,4 @@ public abstract class LR0TestGrammar extends Grammar {
      */
     public abstract ParseState getParseRoot(String sentence);
 
-
-    protected abstract void setUpGenerationBookends(Map<String, Map<String, String[]>> generationBookendMap);
-
-    public String[] getGenerationBookends(String sentence, String language) {
-        String[] bookends = null;
-
-        try {
-            bookends = generationBookendMap.get(language).get(sentence);
-        }
-        catch (NullPointerException e) {
-            throw new UnsupportedSentenceException("langage and generation bookends", sentence);
-        }
-
-        if(bookends == null) {
-            throw new UnsupportedSentenceException("generation bookends", sentence);
-        }
-
-        return bookends;
-    }
-
-
-    protected abstract void setUpRuleConvertors(Map<String, Map<String, Map<ProductionRule, Generator>>> ruleConvertorMap);
-
-    public Map<ProductionRule, Generator> getRuleConvertor(String sentence, String language) {
-        Map<ProductionRule, Generator> ruleConvertor = null;
-
-        try {
-            ruleConvertor = ruleConvertorMap.get(language).get(sentence);
-        }
-        catch (NullPointerException e) {
-            throw new UnsupportedSentenceException("langage and rule convertor", sentence);
-        }
-
-        if(ruleConvertor == null) {
-            throw new UnsupportedSentenceException("rule convertor", sentence);
-        }
-
-        return ruleConvertor;
-    }
-
-
-    protected abstract void setUpCodeGenerations(Map<String, Map<String, String>> codeGenerations);
-
-    public String getGeneratedCode(String sentence, String language) {
-        String generatedCode = null;
-
-        try {
-            generatedCode = codeGenerations.get(language).get(sentence);
-        }
-        catch (NullPointerException e) {
-            throw new UnsupportedSentenceException("langage and code generation", sentence);
-        }
-
-        if(generatedCode == null) {
-            throw new UnsupportedSentenceException("code generation", sentence);
-        }
-
-        return generatedCode;
-    }
 }
