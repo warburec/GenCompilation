8461223cccabdd6a9607100abceb71b3c1c89788
warburec <77669019+warburec@users.noreply.github.com>
2024-01-08 12:53:27 +0000

    Architecture diagram updated

--- tests/test_aids/UnknownGrammarException.java
+++
@@ -1,7 +0,0 @@
-package tests.test_aids;
-
-public class UnknownGrammarException extends RuntimeException {
-    public UnknownGrammarException() {
-        super("The grammar requested is unsupported");
-    }
-}
