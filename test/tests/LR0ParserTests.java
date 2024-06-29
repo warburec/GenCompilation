9d25a9e3eff52e2f500a48ef38ad549b176873df
warburec <77669019+warburec@users.noreply.github.com>
2024-06-29 18:50:34 +0100

    Update build.gradle and tests to JUnit5 (#29)

--- test/tests/LR0ParserTests.java
+++ test/tests/LR0ParserTests.java
@@ -1,11 +1,10 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.*;
-
-import org.junit.Test;
-
 import grammar_objects.*;
 import syntax_analysis.grammar_structure_creation.*;
 import syntax_analysis.parsing.*;
