ec2638e6343472db48efe3e6876eac5aab201b26
warburec <77669019+warburec@users.noreply.github.com>
2025-01-24 22:27:14 +0000

    StringFileEditor reading contents

---
+++ src/main/java/storage/fileEditors/StringFileEditor.java
@@ -0,0 +1,27 @@
+package storage.fileEditors;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+
+public class StringFileEditor extends FileEditor<String> {
+
+    @Override
+    public void store(String path, String contents) throws UncheckedIOException {
+        try {
+            Files.writeString(Paths.get(path), contents);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to store content to \"" + path + "\"", e);
+        }
+    }
+
+    @Override
+    public String readFrom(String path) throws UncheckedIOException {
+        try {
+            return Files.readString(Paths.get(path), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to read the contents from \"" + path + "\"", e);
+        }
+    }
+    
+}
