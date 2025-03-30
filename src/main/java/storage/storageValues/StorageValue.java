fcd291b0defcad93cc209f62f38abca87e15187c
warburec <77669019+warburec@users.noreply.github.com>
2025-03-30 14:50:00 +0100

    StorageValue .equals and tests

--- src/main/java/storage/storageValues/StorageValue.java
+++ src/main/java/storage/storageValues/StorageValue.java
@@ -10,4 +10,15 @@ public abstract class StorageValue<T> {
     public T getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        StorageValue<?> other = (StorageValue<?>) obj;
+        if (value == null) return other.value == null;
+        return value.equals(other.value);
+    }
 }
---
+++ src/test/java/storage/storageValues/StorageValueTests.java
@@ -0,0 +1,54 @@
+package storage.storageValues;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class StorageValueTests {
+
+    @Test
+    public void nonEquality() {
+        String strValue = "0";
+        Integer intValue = Integer.parseInt(strValue);
+
+        StringStorageValue strStorage = new StringStorageValue(strValue);
+        IntegerStorageValue intStorage = new IntegerStorageValue(intValue);
+
+        assertNotEquals(strStorage, intStorage);
+    }
+
+    @Test
+    public void equality() {
+        String strValue = "0";
+        Integer intValue = Integer.parseInt(strValue);
+
+        StringStorageValue strStorage1 = new StringStorageValue(strValue);
+        IntegerStorageValue intStorage1 = new IntegerStorageValue(intValue);
+
+        StringStorageValue strStorage2 = new StringStorageValue(strValue);
+        IntegerStorageValue intStorage2 = new IntegerStorageValue(intValue);
+
+        assertEquals(strStorage1, strStorage2);
+        assertEquals(intStorage1, intStorage2);
+    }
+
+    @Test
+    public void nullNonEquality() {
+        String strValue = null;
+        Integer intValue = null;
+
+        StringStorageValue strStorage = new StringStorageValue(strValue);
+        IntegerStorageValue intStorage = new IntegerStorageValue(intValue);
+
+        assertNotEquals(strStorage, intStorage);
+    }
+
+    @Test
+    public void nullEquality() {
+        String strValue = null;
+
+        StringStorageValue strStorage1 = new StringStorageValue(strValue);
+        StringStorageValue strStorage2 = new StringStorageValue(strValue);
+
+        assertEquals(strStorage1, strStorage2);
+    }
+}
