68aedb8dda6dc263dcdf183aba26599706ef5e37
warburec <77669019+warburec@users.noreply.github.com>
2025-04-07 22:16:50 +0100

    GrammarBuilder

---
+++ src/main/java/component_construction/builders/grammar_objects/GrammarBuilder.java
@@ -0,0 +1,77 @@
+package component_construction.builders.grammar_objects;
+
+import java.util.*;
+import grammar_objects.*;
+
+//TODO: Test
+public class GrammarBuilder {
+
+    protected Set<Token> tokens = new HashSet<>();
+    protected Set<NonTerminal> nonTerminals = new HashSet<>();
+    protected List<ProductionRule> productionRules = new ArrayList<>();
+    protected NonTerminal sentinal;
+
+    /**
+     * Sets up the sentinal for the grammar being built
+     * @param sentinal The sentinal non-terminal
+     * @return This builder for method chaining
+     */
+    public GrammarBuilder setSentinal(NonTerminal sentinal) {
+        this.sentinal = sentinal;
+        return this;
+    }
+
+    /**
+     * Adds a production rule for the grammar being built
+     * @param productionRule The production rule
+     * @return This builder for method chaining
+     */
+    public GrammarBuilder addRule(ProductionRule productionRule) {
+        addTokens(productionRule);
+        productionRules.add(productionRule);
+        return this;
+    }
+
+    /**
+     * Adds a production rule for the grammar being built
+     * @param nonTerminal The NonTerminal for the rule
+     * @param elements In-order elements for the rule
+     * @return This builder for method chaining
+     */
+    public GrammarBuilder addRule(NonTerminal nonTerminal, LexicalElement[] elements) {
+        return addRule(new ProductionRule(nonTerminal, elements));
+    }
+
+    /**
+     * Adds a production rule for the grammar being built
+     * @param nonTerminalName The name of the NonTerminal for this rule
+     * @param elements In-order elements for this rule
+     * @return This builder for method chaining
+     */
+    public GrammarBuilder addRule(String nonTerminalName, LexicalElement[] elements) {
+        return addRule(new NonTerminal(nonTerminalName), elements);
+    }
+
+    /**
+     * Adds a collection of production rules to the grammar being built
+     * @param rules The collection of rules
+     * @return This builder for method chaining
+     */
+    public GrammarBuilder addRules(Collection<ProductionRule> rules) {
+        for (ProductionRule productionRule : rules) {
+            addRule(productionRule);
+        }
+
+        return this;
+    }
+
+
+    private void addTokens(ProductionRule productionRule) {
+        nonTerminals.add(productionRule.nonTerminal());
+
+        for (LexicalElement element : productionRule.productionSequence()) {
+            if (element instanceof Token) { tokens.add((Token)element); }
+            else if (element instanceof NonTerminal) { nonTerminals.add((NonTerminal)element); }
+        }
+    }
+}
