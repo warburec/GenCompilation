7a83abdb4f7c9b61f6e57a1cbc96477362405364
warburec <77669019+warburec@users.noreply.github.com>
2026-07-26 18:27:26 +0100

    LoadableBy getTargetLoader rework

--- src/main/java/component_construction/storage/dynamic_loading/LoadableBy.java
+++ src/main/java/component_construction/storage/dynamic_loading/LoadableBy.java
@@ -2,35 +2,41 @@ package component_construction.storage.dynamic_loading;
 
 import java.lang.reflect.*;
 
+import component_construction.storage.exceptions.IncorrectlyFormattedException;
+
 /**
  * A class specifying a designated loader for the current class.
  * Loaders will be loaded using reflection.
- * @param <T>
+ * @param <L> The concrete Loader class
  */
 public interface LoadableBy<L extends Loader<?>> {
 
     @SuppressWarnings("unchecked")
-    public static <T> Class<Loader<T>> getTargetLoader(Class<LoadableBy<Loader<T>>> loadableByClass) {
+    public static <T, L extends Loader<T>> Class<L> getTargetLoader(Class<? extends LoadableBy<L>> loadableByClass) throws IllegalArgumentException, IncorrectlyFormattedException {
+        ParameterizedType loadableByInterface = null;
+
         for (Type currentInterface : loadableByClass.getGenericInterfaces()) {
             if (!(currentInterface instanceof ParameterizedType)) continue;
 
             ParameterizedType paramType = (ParameterizedType) currentInterface;
-            
             if (!paramType.getRawType().equals(LoadableBy.class)) continue;
             
-            Type actualTypeArgument = paramType.getActualTypeArguments()[0];
-            
-            //TODO: Scrutinise below code, along with warning supression
-
-            // If T is a standard class/interface, return it
-            if (actualTypeArgument instanceof Class)
-                return (Class<Loader<T>>) actualTypeArgument;
-            // Handle nested generic loaders (e.g., MyLoader<String>)
-            else if (actualTypeArgument instanceof ParameterizedType)
-                return (Class<Loader<T>>) ((ParameterizedType) actualTypeArgument).getRawType();
+            loadableByInterface = paramType;
+            break;
         }
 
-        throw new IllegalArgumentException("Class " + loadableByClass.getName() + " does not directly implement LoadableBy with a concrete generic type.");
-    }
+        if (loadableByInterface == null)
+            throw new IllegalArgumentException("Class " + loadableByClass.getName() + " does not directly implement LoadableBy.");
 
+        Type actualTypeArgument = loadableByInterface.getActualTypeArguments()[0];
+
+        if (actualTypeArgument instanceof ParameterizedType)
+            throw new IncorrectlyFormattedException("Class " + loadableByClass.getName() + " provides a nested generic loader (" + actualTypeArgument.getTypeName() + "). Loaders must be direct, concrete Class objects.");
+        
+        if (!(actualTypeArgument instanceof Class))
+            throw new IncorrectlyFormattedException("Class " + loadableByClass.getName() + " provides an abstract or unbound type token (" + actualTypeArgument.getTypeName() + "). The loader must be a direct, concrete Class object.");
+
+        return (Class<L>) actualTypeArgument;
+    }
+    
 }
