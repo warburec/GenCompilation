553a817584277d639477ab57a9a2c57844a9da00
warburec <77669019+warburec@users.noreply.github.com>
2026-08-22 00:24:09 +0100

    StorableCompilerBuilder imports fix

--- src/main/java/component_construction/storage/builders/StorableCustomCompilerBuilder.java
+++ src/main/java/component_construction/storage/builders/StorableCustomCompilerBuilder.java
@@ -1,12 +1,8 @@
 package component_construction.storage.builders;
 
-import code_generation.CodeGenerator;
 import component_construction.builders.CompilerBuilderTemplate;
-import component_construction.custom_components.*;
 import component_construction.storage.StorableCustomCompiler;
 import grammar_objects.GrammarParts;
-import lexical_analysis.LexicalAnalyser;
-import syntax_analysis.SyntaxAnalyser;
 
 public class StorableCustomCompilerBuilder extends CompilerBuilderTemplate<StorableCustomCompilerBuilder, StorableCustomCompiler> {
 
