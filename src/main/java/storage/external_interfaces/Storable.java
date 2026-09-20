fc8954ec3556150673edfdf5c8cab6f4453457a6
warburec <77669019+warburec@users.noreply.github.com>
2026-09-20 23:53:24 +0100

    Storable documentation

--- src/main/java/storage/external_interfaces/Storable.java
+++ src/main/java/storage/external_interfaces/Storable.java
@@ -4,6 +4,10 @@ import storage.storage_values.StorageValue;
 
 public interface Storable {
 
+    /**
+     * Gets the storage representation of this object
+     * @return A {@code StoraageValue} representing the storable state of this object
+     */
     public StorageValue<?> getStorageRepresentation();
     
 }
