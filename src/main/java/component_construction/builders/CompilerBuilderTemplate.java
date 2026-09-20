80d14500bc6a85aafc6a1efaa0d7e4cdd0e7c19e
warburec <77669019+warburec@users.noreply.github.com>
2026-09-20 16:10:42 +0100

    CompilerBuilderTemplate documentation update

--- src/main/java/component_construction/builders/CompilerBuilderTemplate.java
+++ src/main/java/component_construction/builders/CompilerBuilderTemplate.java
@@ -31,10 +31,11 @@ public abstract class CompilerBuilderTemplate <B extends CompilerBuilderTemplate
 
     /**
      * A shortcut setter method for all components necessary for building a compiler
-     * @param lexicalAnalyserFactory
-     * @param syntaxAnalyserFactory
-     * @param codeGeneratorFactory
-     * @param grammarBundle
+     * @param lexicalAnalyserFactory A factory which produces lexical analysers
+     * @param syntaxAnalyserFactory A factory which produces syntax analysers
+     * @param codeGeneratorFactory A factory which produces code generators
+     * @param grammarBundle A {@code GrammarBundle} for the grammar to be compiled
+     * @return This object for method chaining
      */
     public B setComponents(
         LexicalAnalyserFactory lexicalAnalyserFactory,
@@ -57,15 +58,16 @@ public abstract class CompilerBuilderTemplate <B extends CompilerBuilderTemplate
     
     /**
      * A shortcut setter method for all components necessary for building a compiler
-     * @param lexicalAnalyserFactory
-     * @param syntaxAnalyserFactory
-     * @param codeGeneratorFactory
-     * @param grammar
-     * @param ruleConvertor
-     * @param whitespaceDelimiters May be null
-     * @param stronglyReservedWords May be null
-     * @param weaklyReservedWords May be null
-     * @param dynamicTokenRegex May be null
+     * @param lexicalAnalyserFactory A factory which produces lexical analysers
+     * @param syntaxAnalyserFactory A factory which produces syntax analysers
+     * @param codeGeneratorFactory A factory which produces code generators
+     * @param grammar The {@code grammar} to be used for compilation
+     * @param ruleConvertor The {@code RuleConvertor} to produce output code from compiled input sentences
+     * @param whitespaceDelimiters An array of strings to be conidered as whitespace in compiled sentences. May be null
+     * @param stronglyReservedWords An array of strings which may only be used by reserved words within the provided grammar. May be null
+     * @param weaklyReservedWords An array of strings to be used by reserved words within the provided grammar, they may appear as part of other tokens. May be null
+     * @param dynamicTokenRegex An array of {@code DynamicTokenRegex} for grammar tokens which may take different forms according to provided regex. May be null
+     * @return This object for method chaining
      */
     public B setComponents(
         LexicalAnalyserFactory lexicalAnalyserFactory,
@@ -92,59 +94,108 @@ public abstract class CompilerBuilderTemplate <B extends CompilerBuilderTemplate
     }
     
 
+    /**
+     * Sets the lexical analyser for the produced compiler
+     * @param factory A factory which produces lexical analysers
+     * @return This object for method chaining
+     */
     public B setLexicalAnalyser(LexicalAnalyserFactory factory) {
         this.lexicalAnalyserFactory = factory;
         return getThis();
     }
 
+    /**
+     * Sets the syntax analyser for the produced compiler
+     * @param factory A factory which produces syntax analysers
+     * @return This object for method chaining
+     */
     public B setSyntaxAnalyser(SyntaxAnalyserFactory factory) {
         this.syntaxAnalyserFactory = factory;
         return getThis();
     }
 
+    /**
+     * Sets the code generator for the produced compiler
+     * @param factory A factory which produces code generators
+     * @return This object for method chaining
+     */
     public B setCodeGenerator(CodeGeneratorFactory factory) {
         this.codeGeneratorFactory = factory;
         return getThis();
     }
 
 
+    /**
+     * Sets the grammar for the produced compiler
+     * @param grammar The {@code grammar} to be used for compilation
+     * @return This object for method chaining
+     */
     public B setGrammar(Grammar grammar) {
         this.grammar = grammar;
         return getThis();
     }
 
+    /**
+     * Sets the rule convertor for the produced compiler
+     * @param ruleConvertor The {@code RuleConvertor} to produce output code from compiled input sentences
+     * @return This object for method chaining
+     */
     public B setRuleConvertor(RuleConvertor ruleConvertor) {
         this.ruleConvertor = ruleConvertor;
         return getThis();
     }
 
-    
+
+    /**
+     * Sets the designated whitespace delimiters for compilation
+     * @param whitespaceDelimiters An array of strings to be conidered as whitespace in compiled sentences
+     * @return This object for method chaining
+     */
     public B setWhitespaceDelimiters(String[] whitespaceDelimiters) {
         this.whitespaceDelimiters = whitespaceDelimiters;
         return getThis();
     }
 
+    /**
+     * Sets designated reserved words for compilation. These may not be used within other tokens
+     * @param stronglyReservedWords An array of strings which may only be used by reserved words within the provided grammar
+     * @return This object for method chaining
+     */
     public B setStronglyReservedWords(String[] stronglyReservedWords) {
         this.stronglyReservedWords = stronglyReservedWords;
         return getThis();
     }
 
+    /**
+     * Sets reserved words for compilation that may be used as part of other tokens
+     * @param weaklyReservedWords An array of strings to be used by reserved words within the provided grammar, they may appear within other tokens
+     * @return This object for method chaining
+     */
     public B setWeaklyReservedWords(String[] weaklyReservedWords) {
         this.weaklyReservedWords = weaklyReservedWords;
         return getThis();
     }
 
+    /**
+     * Sets the dynaic tokens for comiplation
+     * @param dynamicTokenRegex An array of {@code DynamicTokenRegex} for grammar tokens which may take different forms according to provided regex
+     * @return This object for method chaining
+     */
     public B setDynamicTokenRegex(DynamicTokenRegex[] dynamicTokenRegex) {
         this.dynamicTokenRegex = dynamicTokenRegex;
         return getThis();
     }
 
-    
+
+    /**
+     * Creates a new compiler according to the components provided to this builder
+     * @return The produced compiler
+     */
     public abstract C createCompiler();
 
 
     /**
-     * A simple helper method to be filled with "return this;" by subclasses of this builder
+     * A simple helper method to be filled with "{@code return this;}" by subclasses of this builder
      * @return The current builder object "this"
      */
     protected abstract B getThis();
